b9f1d7822ac28b5cc5e63e305b520164c44324a8
irikaK <kati2944@lgsstudent.org>
2020-08-09 00:37:57 -0700

    finished for loops and comments page

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -9,7 +9,41 @@ class Comments extends React.Component{
 	render(){
 		return (
 			<div>
-				This is the Comments content
+				<b>comments</b> are used to <b>write notes</b> next to your code <b>that aren't executed when you run your code</b>. 
+				if, on the <b className="pageName">variables page</b>, you saw notes denoted with two forward slashes, those were comments!
+				<br/><br/>
+				there are two types of comments you can make:
+				<ul>
+					<li>single-line comments</li>
+					<li>multi-line comments</li>
+				</ul>
+				<h3>wait, so how do you make a comment?</h3>
+				here's the blueprint on how to make a single-line comment:<br/>
+				<b>//<i>&lt;your note here&gt;</i></b><br/><br/>
+				and here's the blueprint on how to make a multi-line comment:<br/>
+				<b>/*<br/>
+				<p><i>&lt;your note here&gt;</i></p><br/>
+				*/</b><br/>
+				<h3>ok, but what's the point of that?</h3>
+				commenting your code is a <b>really good habit</b> that you should start right now. like seriously. go. i'm waiting... still here. waiting. or shall i say, //waiting. <br/>
+				in all seriousness, it's really helpful for you and others who might be reading your code to <b>understand what certain lines of code do</b>, improving your code's <b>readability</b>.
+				<h3>here's an example</h3>
+				<div className="code">
+					boolean understandComments = false; <div className="comment">//starts out not understanding comments</div>
+					<div className="comment">
+						/*<br/>
+						<p>because you've read the information above, you now understand comments and how important they are. so, we can set understandComments to true!</p><br/>
+						*/
+					</div><br/>
+					understandComments = true;
+				</div>
+				in the code above, you can see both single-line and multi-line comments being used.
+				<h3>//is it already time for recap?</h3>
+				<b>yayyyy!</b> you made it to reacap!<br/><br/>
+				so, just to sum up, the two types of comments are <b>single-line comments</b>, denoted by <b>//</b>, and <b>multi-line comments</b>, denoted by <b>/* ... */</b>. <br/><br/>
+				comments are <b>really important</b> because they help increase your code's <b>readability</b>. now, going forward, be sure to comment on your code! :)
+				<br/><br/>
+				...we know <b className="note">just for java</b> is as <b>addicting</b> as the newest Netflix binge, so what are you waiting for? <b className="pageName">keep coding!</b>
 			</div>
 		)
 	}
--- src/components/ForLoop.js
+++ src/components/ForLoop.js
@@ -26,19 +26,24 @@ class ForLoop extends React.Component{
 				<h3>what in the world is initialize?</h3>
 				good question! initializing is the <b>first component</b> of your for loop. it works to <b>create the variable</b> that you will use to go through your for loop.
 				for initalizing, we always use an <b>int variable</b>, often denoted by the letter <b>i</b>.  
-				in the initialize section, we create i and set it equal to the <b>start value</b>. for example, if you want your for loop to print the numbers 1-5, you would <b>set i equal to 1</b>, because that's the number you want to print first.
+				in the initialize section, we create i and set it equal to the <b>start value</b>. <br/>
+				for example, if you want your for loop to print the numbers 1-5, you would <b>set i equal to 1</b>, because that's the number you want to print first.
 				(<b className="note">psst. need a reminder</b> on how to create a variable? check out the <b className="pageName">variables page</b>!)
 
 				<h3>and what's condition?</h3>
 				the condition is the <b>second component</b>. it works to <b>determine when the loop should stop</b>. you can denote this using <b>&lt;</b>, <b>&le;</b>, <b>&gt;</b>, or <b>&ge;</b> depending on what you want your loop to do.
-				going back to our example of printing out the numbers 1-5, we can make our condition in two ways: <b>i &lt; 6</b> or <b>i &le; 5</b>. which one you choose is up to you.
+				<br/>going back to our example of printing out the numbers 1-5, we can make our condition in two ways: <b>i &le; 5</b> or <b>i &lt; 6</b>. which one you choose is up to you.
 				
 				<h3>hey, wait! you still have to explain iterate!</h3>
-				don't worry, we didn't forget :). the iterate portion is our <b>last component</b> and works to <b>increment your integer variable</b>, <b>either adding one or subtracting one</b> depending on the direction you would like your loop to go in. 
-				if you want your integer to be incremented by +1 each time it loops, you type <b>i++</b>. if you want your integer to be incremented by -1 each time it loops, you type <b>i--</b>.
-				going back to our example of printing numbers 1-5, we would want to increase i by 1 each time. so, for our last component of the for loop, we would type <b>i++</b>.<br/>
-				(<b className="note">btw:</b> don't forget the semicolons between each of the three components)
-				<br/><br/>here is our example fully coded out: 
+				don't worry, we didn't forget :). the iterate portion is our <b>last component</b> and works to <b>increment your integer variable</b>. you can increment by however much you want, <b>either increasing or decreasing</b> depending on the direction you would like your loop to go in. 
+				if you want your integer to be increased by a certain number, you would type <b>i += <i>&lt;number you want to increase by each time&gt;</i></b>. 
+				if you want your integer to be decreased by a certain number, you would type <b>i -= <i>&lt;number you want to decrease by each time&gt;</i></b>.
+				a shortcut for increasing i by 1 each time is typing <b>i++</b>, while a shortcut for decreasing i by 1 is... you guessed it: <b>i--</b>.
+				you can also use multiplication (<b>i *=</b>)and division (<b>i /=</b>), but addition and subtraction are the most common.
+				<br/>going back to our example of printing numbers 1-5, we would want to increase i by 1 each time. so, for our last component of the for loop, we would type <b>i++</b>.
+				(<b className="note">btw:</b> don't forget the semicolons between each of the three components.)
+				<h3>and for some examples</h3>
+				here is our example from above fully coded out: 
 				<div className="code">
 					for(int i = 1; i &le; 5; i++) &#123; <br/>
 						<p>System.out.print(i + " ")</p> <div className = "comment">//the " " is to add a space between the numbers when printing</div><br/>
@@ -48,7 +53,32 @@ class ForLoop extends React.Component{
 				<div className="code">
 					1 2 3 4 5
 				</div>
-				yay! now you know all about for loops!
+				<h3>and here's another example:</h3>
+				in this example, we want to use a for loop to add up the powers of 2 between 1 and 100. sounds difficult, i know, but let's break down the problem.<br/>
+				remember, the first thing we want to do is <b>make our integer i</b>. (<b className="note">hint:</b> think about what number we want to start at.) 
+				next, we want our <b>condition</b>. (<b className="note">hint:</b> there are two ways to write this part.)
+				and lastly, we will need to <b>increment our integer</b> in a way to get the <b>powers of 2</b>. i'll let you think of that one for a bit. (<b className="note">hint:</b> think back to your math class.)<br/> 
+				now, inside the for loop we will need a way to add our number to a sum... hmm... might need to remember some things from the <b className="pageName">variables page</b> for this part.
+				ok, got it? let's see it coded out!
+				<div className="code">
+					int sum = 0; <div className = "comment">//this variable will keep track of our sum in our for loop</div><br/>
+					for(int i = 1; i &le; 100; i*=2)&#123;<br/>
+						<p>sum += i; <div className="comment">//+= is a shortcut for saying sum = sum + i.</div></p><br/>
+					&#125;
+				</div>
+				<b>i*=2</b> works by multiplying i by 2 each time it iterates through the for loop. so, with this in mind, i would start at 1, then 2, then 4, then 8, then 16, and so on.<br/>
+				<br/>yay! now you know all about for loops!
+				<h3>is it really time for reacap? get it. for. hehe.</h3>
+				<b>ayyyyyy!</b> you made it to recap!<br/><br/>
+				now, i know that was a lot, so here's a lil' summary:<br/>
+				for loops help to loop through code a <b>specific number of times</b>.<br/>
+				there are three parts to a for loop: 
+				<ol>
+					<li><b>initialize</b>, when you make your integer <b>i</b> and set it equal to the start number</li>
+					<li><b>condition</b>, where you decide <b>how</b> and <b>when</b> your loop should stop using the different signs</li>
+					<li><b>iterate</b>, where you <b>increment</b> your integer a specific amount each time it loops through your code</li>
+				</ol>
+
 			</div>
 		)
 	}
