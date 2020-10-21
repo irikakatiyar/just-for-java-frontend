a60d61eeb94c9ea4bd02eee9fdbfb4bc243772bf
Irith Katiyar <irith@katiyar.com>
2020-10-20 17:21:11 -0700

    css applied to email

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -10,7 +10,7 @@ class ContactUs extends React.Component{
 		return (
 			<div>
 				i guess we should put some contact info here...
-				<br/><br/> have any comments or questions? have any feedback or suggestions? don't hesitate to <b>email us</b> at <a href="mailto:justforjavaco@gmail.com">justforjavaco@gmail.com</a>!
+				<br/><br/> have any comments or questions? have any feedback or suggestions? don't hesitate to <b>email us</b> at <a className="email" href="mailto:justforjavaco@gmail.com">justforjavaco@gmail.com</a>!
 			</div>
 		)
 	}
